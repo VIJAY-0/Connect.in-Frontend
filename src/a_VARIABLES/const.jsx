19cafd683ab1ff3f59d38cc6f12d2f83f3813ba5
VIJAY-0 <99347482+VIJAY-0@users.noreply.github.com>
2025-01-29 02:28:03 +0530

    Update const.jsx

--- src/a_VARIABLES/const.jsx
+++ src/a_VARIABLES/const.jsx
@@ -1,5 +1,5 @@
 // src/constants.js
-export const API_BASE_URL = 'http://13.61.200.43:8000/';
+export const API_BASE_URL = 'https://connect-in-backend.onrender.com';
 export const APP_NAME = 'My React App';
 export const THEME_COLOR = '#3498db';
 // Add more common variables here
